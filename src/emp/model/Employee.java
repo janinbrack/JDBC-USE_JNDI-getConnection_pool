2daa1cd2e439d14c023d17e7217a2704692e6a4b
Kaohsiung_Bowen <50541901+janinbrack@users.noreply.github.com>
2021-06-14 21:27:30 +0800

    add Javax Persistence API  2.2
    use @Entity and @Table

--- src/emp/model/Employee.java
+++ src/emp/model/Employee.java
@@ -1,9 +1,18 @@
 package emp.model;
 
 import java.io.Serializable;
+import javax.persistence.*;
 
+
+@Entity
+@Table(name = "employe")
 public class Employee implements Serializable {
+
+	
+@Id
+@Column(name = "id")
 private int id;
+
 private String name;
 private int gender;
 private String email;
